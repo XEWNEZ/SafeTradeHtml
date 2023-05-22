7fa71ae61d4357be84cb02dc3ed1228c6c5228d1
Eren Tuten <tuten1636@gmail.com>
2023-05-22 14:33:04 +0300

    Add project files.

---
+++ SafeTradeHtml/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SafeTradeHtml.Models;
+using System.Diagnostics;
+
+namespace SafeTradeHtml.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Route(""),Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("our-services")]
+        public IActionResult Service()
+        {
+            return View();
+        }
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [Route("about-us")]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SafeTradeHtml/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SafeTradeHtml.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
